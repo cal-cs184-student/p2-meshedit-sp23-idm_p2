9664f39e0dacc1a0f102536ff5d75d4da4d2b3b1
JamesYi2953 <yipuyuan@berkeley.edu>
2023-02-17 14:12:15 -0800

    finish 3,4,5 and write up

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -112,22 +112,200 @@ namespace CGL
     // Returns an approximate unit normal at this vertex, computed by
     // taking the area-weighted average of the normals of neighboring
     // triangles, then normalizing.
-    return Vector3D();
+      Vector3D res;
+      Vector3D position = this->position;
+      HalfedgeCIter h = this -> halfedge();      // get the outgoing half-edge of the vertex
+      Vector3D last_position;
+      Vector3D vector1;
+      Vector3D vector2;
+      Vector3D face_normal;
+      float area;
+      auto get_area = [&](Vector3D p1, Vector3D p2, Vector3D p3) {
+          // a function that calculated the area of a mesh given by three position
+          Vector3D line1 = p2 - p1;
+          Vector3D line2 = p3 - p2;
+          Vector3D line3 = p1 - p3;
+          float line1_length = sqrt(pow(line1.x,2) + pow(line1.y, 2) + pow(line1.z, 2));
+          float line2_length = sqrt(pow(line2.x, 2) + pow(line2.y, 2) + pow(line2.z, 2));
+          float line3_length = sqrt(pow(line3.x, 2) + pow(line3.y, 2) + pow(line3.z, 2));
+          float area;
+          float p = (line1_length + line2_length + line3_length);
+          area = sqrt(p * (p - line1_length) * (p - line2_length) * (p - line3_length));
+          return area;
+      };
+
+      auto normalize = [&](Vector3D p) {
+          // a normalize function
+          float line_length = sqrt(pow(p.x, 2) + pow(p.y, 2) + pow(p.z, 2));
+          p = p / line_length;
+          return p;
+      };
+
+      do {
+          HalfedgeCIter h_twin = h -> twin(); // get the opposite half-edge
+          VertexCIter v = h_twin -> vertex(); // vertex is the 'source' of the half-edge, so
+          if (h == this->halfedge())
+          {
+              last_position = v->position;
+          }
+          else
+          {
+              vector1 = position - last_position;
+              vector2 = v->position - last_position;
+              face_normal = cross(vector1, vector2);
+              area = get_area(position, last_position, v->position);
+              res = res + area * face_normal;
+              last_position = v->position;
+
+          }
+          Vector3D last_position = v->position;
+          // h->vertex() is v, whereas h_twin->vertex()
+          // is the neighboring vertex
+          //cout << v -> position << endl;      // print the vertex position
+          h = h_twin -> next();               // move to the next outgoing half-edge of the vertex
+      } while (h != this->halfedge());          // keep going until we are back where we were
+    return normalize(res);
   }
 
   EdgeIter HalfedgeMesh::flipEdge( EdgeIter e0 )
   {
+      HalfedgeIter h1 = e0->halfedge();
+      if (h1->isBoundary())
+      {
+          return e0;
+      }
+      HalfedgeIter h2 = h1->next();
+      HalfedgeIter h3 = h2->next();
+      HalfedgeIter h4 = h1->twin();
+      HalfedgeIter h5 = h4->next();
+      HalfedgeIter h6 = h5->next();
+      //Vertices
+      VertexIter a = h1->vertex();
+      VertexIter b = h2->vertex();
+      VertexIter d = h3->vertex();
+      VertexIter c = h6->vertex();
+      //Faces
+      FaceIter f0 = h1->face(); 
+      FaceIter f1 = h4->face(); 
+      //Edges
+      EdgeIter bd = h2->edge();
+      EdgeIter da = h3->edge();
+      EdgeIter ac = h5->edge();
+      EdgeIter cb = h6->edge();
+
+      h1->setNeighbors(h3, h4, c, e0, f0);
+      h2->setNeighbors(h4, h2->twin(), b, bd, f1);
+      h3->setNeighbors(h5, h3->twin(), d, da, f0);
+      h4->setNeighbors(h6, h1, d, e0, f1);
+      h5->setNeighbors(h1, h5->twin(), a, ac, f0);
+      h6->setNeighbors(h2, h6->twin(), c, cb, f1);
     // TODO Part 4.
     // This method should flip the given edge and return an iterator to the flipped edge.
-    return EdgeIter();
+      a->halfedge() = h5;
+      b->halfedge() = h2;
+      c->halfedge() = h6;
+      d->halfedge() = h3;
+
+      e0->halfedge() = h1;
+      bd->halfedge() = h2;
+      da->halfedge() = h3;
+      ac->halfedge() = h5;
+      cb->halfedge() = h6;
+
+      f0->halfedge() = h1;
+      f1->halfedge() = h4;
+      e0 = h1->edge();
+     
+    return e0;
   }
 
   VertexIter HalfedgeMesh::splitEdge( EdgeIter e0 )
   {
+      VertexIter m  = newVertex();
     // TODO Part 5.
     // This method should split the given edge and return an iterator to the newly inserted vertex.
     // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
-    return VertexIter();
+      HalfedgeIter h1 = e0->halfedge();
+      if (h1->isBoundary())
+      {
+          return m;
+      }
+      HalfedgeIter h2 = h1->next();
+      HalfedgeIter h3 = h2->next();
+      HalfedgeIter h4 = h1->twin();
+      HalfedgeIter h5 = h4->next();
+      HalfedgeIter h6 = h5->next();
+      //Vertices
+      VertexIter a = h1->vertex();
+      VertexIter b = h2->vertex();
+      VertexIter d = h3->vertex();
+      VertexIter c = h6->vertex();
+      //Faces
+      FaceIter f0 = h1->face();
+      FaceIter f1 = h4->face();
+      //Edges
+      EdgeIter bd = h2->edge();
+      EdgeIter da = h3->edge();
+      EdgeIter ac = h5->edge();
+      EdgeIter cb = h6->edge();
+
+      // following is what we did
+      Vector3D p1 = a->position;
+      Vector3D p2 = b->position;
+      m->position = (p1 + p2) / 2;
+      HalfedgeIter h7 = newHalfedge();
+      HalfedgeIter h8 = newHalfedge();
+      HalfedgeIter h9 = newHalfedge();
+      HalfedgeIter h10 = newHalfedge();
+      HalfedgeIter h11 = newHalfedge();
+      HalfedgeIter h12 = newHalfedge();
+      FaceIter f2 = newFace();
+      FaceIter f3 = newFace();
+      //EdgeIter ma = newEdge(); e0 = ma!
+      EdgeIter mb = newEdge();
+      EdgeIter mc = newEdge();
+      EdgeIter md = newEdge();
+      h1->setNeighbors(h2, h4, m, mb, f1);
+      h2->setNeighbors(h7, h2->twin(), b, bd, f1);
+      h3->setNeighbors(h9, h3->twin(), d, da, f2);
+      h4->setNeighbors(h12, h1, b, mb, f0);
+      h5->setNeighbors(h11, h5->twin(), a, ac, f3);
+      h6->setNeighbors(h4, h6->twin(), c, cb, f0);
+      h7->setNeighbors(h1, h8, d, md, f1);
+      h8->setNeighbors(h3, h7, m, md, f2);
+      h9->setNeighbors(h8, h10, a, e0, f2);
+      h10->setNeighbors(h5, h9, m, e0, f3);
+      h11->setNeighbors(h10, h12, c, mc, f3);
+      h12->setNeighbors(h6, h11, m, mc, f0);
+      // assign to vertex
+      m->halfedge() = h1;
+      a->halfedge() = h5;
+      b->halfedge() = h2;
+      c->halfedge() = h6;
+      d->halfedge() = h3;
+      // assign to edge
+      bd->halfedge() = h2;
+      da->halfedge() = h3;
+      ac->halfedge() = h5;
+      cb->halfedge() = h6;
+      e0->halfedge() = h10;
+      mb->halfedge() = h1;
+      mc->halfedge() = h12;
+      md->halfedge() = h8;
+      // assign the face
+      f0->halfedge() = h4;
+      f1->halfedge() = h1;
+      f2->halfedge() = h3;
+      f3->halfedge() = h5;
+
+      // new judge
+      m->isNew = true;
+      e0->isNew = false;
+      mc->isNew = true;
+      mb->isNew = false;
+      md->isNew = true;
+
+    return m;
   }
 
 
